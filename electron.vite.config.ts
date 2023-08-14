09b3bd94e3cabf0b68111506f1a5c92ac078503f
CPS <holy.dandelion@139.com>
2023-08-14 18:23:20 +0800

    ✨新增(custom): 新增自动导入模块

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,14 @@
 import { resolve } from "path";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import {
+  ElementPlusResolver,
+  TDesignResolver,
+} from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
   main: {
@@ -11,13 +19,43 @@ export default defineConfig({
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: { "@preload": resolve("./src/preload/"), "@": resolve("./src/") },
+    },
   },
   renderer: {
     resolve: {
       alias: {
-        "@renderer": resolve("src/renderer/src"),
+        "@renderer": resolve("./src/renderer/src"),
+        "@": resolve("./src/"),
       },
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ["vue", "vue-router"],
+        dts: resolve("./src/types/auto-imports.d.ts"),
+        resolvers: [
+          ElementPlusResolver(),
+          TDesignResolver({
+            library: "vue-next",
+          }),
+        ],
+      }),
+      Components({
+        dirs: [
+          resolve("./src/renderer/src/components"),
+          resolve("./src/renderer/src/views"),
+        ],
+        dts: resolve("./src/types/auto-components.d.ts"),
+        resolvers: [
+          ElementPlusResolver(),
+          TDesignResolver({
+            library: "vue-next",
+          }),
+        ], // 按需引入同时关联自动导入
+      }),
+    ],
   },
 });
