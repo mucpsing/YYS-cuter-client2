8c4eda3f8b74c67b487b58fa7a675b338a78b269
cpasion-office-win10 <373704015@qq.com>
2025-10-16 09:24:19 +0800

    ✨新增(custom):
    加入任务管理，完善UI

--- src/renderer/src/views/GisApi/body/setp4/setp4.vue
+++ src/renderer/src/views/GisApi/body/setp4/setp4.vue
@@ -2,7 +2,7 @@
  * @Author: cpasion-office-win10 373704015@qq.com
  * @Date: 2024-12-24 17:11:32
  * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-08-19 10:27:03
+ * @LastEditTime: 2025-10-16 09:06:12
  * @FilePath: \yys-cuter-client2\src\renderer\src\views\GisApi\body\setp4\setp4.vue
  * @Description: 这页面用来对输出的图片进行项目范围的绘制，白底红框
 -->
@@ -18,7 +18,7 @@
 </template>
 
 <script setup>
-import TaskList from "./taskManager/taskList.vue"
+// import TaskList from "./taskManager/taskList.vue"
 </script>
 
 <style lang="scss" scoped></style>
--- src/renderer/src/views/GisApi/body/setp4/taskManager/taskList.vue
+++ src/renderer/src/views/GisApi/body/setp4/taskManager/taskList.vue
@@ -1,9 +1,8 @@
-
 <!--
  * @Author: cpasion-office-win10 373704015@qq.com
  * @Date: 2025-08-06 09:19:57
  * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-08-18 16:45:35
+ * @LastEditTime: 2025-10-16 09:03:41
  * @FilePath: \yys-cuter-client2\src\renderer\src\views\GisApi\body\taskManager\taskList.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -18,7 +17,7 @@
       </template>
 
       <template #avatar>
-        <img :class="['w-[56px] h-[56px]']" v-if="taskItem.preview" :src="taskItem.preview"></img>
+        <img :class="['w-[56px] h-[56px]']" v-if="taskItem.preview" :src="taskItem.preview" />
         <t-avatar
           v-else
           image="https://tdesign.gtimg.com/site/avatar-boy.jpg"
@@ -47,7 +46,16 @@
           <t-button v-if="taskItem.status == 'running'" variant="text" shape="square">
             <pause-circle-stroke-icon />
           </t-button>
-          <t-button variant="text" shape="square" style="color: red">
+          <t-button
+            variant="text"
+            shape="square"
+            style="color: red"
+            @click="
+              () => {
+                console.log(taskItem)
+              }
+            "
+          >
             <close-octagon-icon />
           </t-button>
         </div>
